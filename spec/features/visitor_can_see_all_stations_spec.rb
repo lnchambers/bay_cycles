aa3f12a6b8e690a4b5cc245c49c8958e299afee6
Luke Chambers <lukenatchambers@gmail.com>
2018-02-17 12:58:27 -0700

    Visitor can see the stations index and all attributes

--- spec/features/visitor_can_see_all_stations_spec.rb
+++ spec/features/visitor_can_see_all_stations_spec.rb
@@ -5,28 +5,28 @@ describe "As a Visitor" do
     @station = create(:station)
   end
   describe "When I visit the stations index" do
-    it "I see all stations names" do
+    it "I see all station's names" do
       visit stations_path
 
-      expect(page).to have_content(@station.name)
+      expect(page).to have_content("Name: #{@station.name}")
     end
 
-    it "I see all stations names" do
+    it "I see all station's dock counts" do
       visit stations_path
 
-      expect(page).to have_content(@station.dock_count)
+      expect(page).to have_content("Dock Count: #{@station.dock_count}")
     end
 
-    it "I see all stations names" do
+    it "I see all station's cities" do
       visit stations_path
 
-      expect(page).to have_content(@station.city)
+      expect(page).to have_content("City: #{@station.city}")
     end
 
-    it "I see all stations names" do
+    it "I see all station's installation date" do
       visit stations_path
 
-      expect(page).to have_content(@station.installation_date)
+      expect(page).to have_content("Installation Date: #{@station.installation_date}")
     end
   end
 end
