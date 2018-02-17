9a3af814c19ee89a6e85d4c21ae04cb58e8744b2
Luke Chambers <lukenatchambers@gmail.com>
2018-02-17 13:38:31 -0700

    Build out CRUD fuctionality for stations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,7 +1,50 @@
 class StationsController < ApplicationController
+  before_action :find_station, only: [:edit, :show, :update, :destroy]
 
   def index
     @stations = Station.all
   end
-  
+
+  def new
+    @station = Station.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @station = Station.new(station_params)
+    if @station.save
+      redirect_to station_path(@station)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @station.update(station_params)
+      redirect_to station_path(@station)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @station.destroy
+    redirect_to stations_path
+  end
+
+  private
+
+    def station_params
+      params.require(:station).permit(:name, :dock_count, :city, :installation_date)
+    end
+
+    def find_station
+      @station = Station.find(params[:id])
+    end
+
 end
