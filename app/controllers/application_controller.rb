686b7ac26dde1ceb9a89c1fdfd22d156509375da
Luke Chambers <lukenatchambers@gmail.com>
2018-02-17 21:21:41 -0700

    Build out tests for station-dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     render file: "/public/404" unless current_admin?
   end
 
+  def require_user
+    render file: "/public/404" unless current_user
+  end
+
   def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
