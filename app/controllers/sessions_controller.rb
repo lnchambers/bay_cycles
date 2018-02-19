8e44ace974aa786129bdeb3994a9740ed33f09a2
Luke Chambers <lukenatchambers@gmail.com>
2018-02-19 13:43:20 -0700

    Logged in users see logout instead of login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to dashboard_path
     else
       flash[:notice] = "Incorrect credentials. Try again."
       render :new
