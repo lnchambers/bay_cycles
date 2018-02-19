946c90027fb451ccb9fe2a082fb0733196e83a83
heybluguy <heybluguy@yahoo.com>
2018-02-19 16:15:09 -0700

    Add mean temperature.

--- spec/features/conditions/visitor_can_see_all_attributes_in_condition_show_spec.rb
+++ spec/features/conditions/visitor_can_see_all_attributes_in_condition_show_spec.rb
@@ -17,11 +17,11 @@ describe "As a visitor" do
       expect(page).to have_content("Max Temperature: #{@condition.max_temperature}")
     end
 
-    # it "I see the conditions mean temperature" do
-    #   visit conditions_path
+    it "I see the conditions mean temperature" do
+      visit conditions_path
 
-    #   expect(page).to have_content("Mean Temperature: #{@condition.mean_temperature}")
-    # end
+      expect(page).to have_content("Mean Temperature: #{@condition.mean_temperature}")
+    end
 
     # it "I see the conditions min temperature" do
     #   visit conditions_path
