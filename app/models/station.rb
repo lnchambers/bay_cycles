840bba6c29420219c8854c65817791ccfed3ebc2
Luke Chambers <lukenatchambers@gmail.com>
2018-02-19 12:40:09 -0700

    Build out tests and set routes

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,6 @@
 class Station < ApplicationRecord
   extend FriendlyId
-  friendly_id :name, use: [:slugged, :finders]
+  friendly_id :name, use: :slugged
 
   validates_presence_of :name, :dock_count, :city, :installation_date
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
 
+  root "welcome#index"
+
   resources :stations
 
+  resources :users, only: [:new]
+
   get "/stations-dashboard", :to => "stations_dashboard#index"
+
+  get "/dashboard", :to => "users#show"
+
+  get "/login", to: "sessions#new"
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
 
 end
