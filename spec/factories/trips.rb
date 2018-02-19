3dc24c7dd69ea69461a1e62c253fd7bd2be8f51a
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 16:49:56 -0700

    Fix Trip Factory and spec. NOTE must create factory station when testing trips.

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+
   factory :trip do
     duration 1
     start_date "2018-02-19 12:27:55"
@@ -6,7 +7,7 @@ FactoryBot.define do
     bike_id 1
     subscription_type "MyString"
     zip_code 1
-    start_station_id station
-    end_station_id station, name: "Station 2"
+    start_station_id 1
+    end_station_id 1
   end
 end
--- spec/features/trips/user_can_see_all_trips_spec.rb
+++ spec/features/trips/user_can_see_all_trips_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe "As a visitor" do
   before :each do
-    
+    @station = create(:station)
     @trips = create_list(:trip, 60)
   end
   describe "when I visit trips index" do
     it "I see first 30 trips" do
       visit trips_path
 
-      expect(page).to have_content(@trips[0..30].each {|trip| trip.duration})
+      expect(page).to have_content(@trips[0..30].map {|trip| trip.duration})
     end
   end
 end
