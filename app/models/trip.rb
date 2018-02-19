e4d3c9bd64f7f3f737075d59c9d82f0b5016e836
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 14:20:09 -0700

    Add multiple associations on Trips from Stations via the model level. Passing test.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,6 @@
 class Trip < ApplicationRecord
   validates_presence_of :duration, :start_date, :end_date, :bike_id, :subscription_type, :zip_code
+
+  belongs_to :start_station, class_name: 'Station'
+  belongs_to :end_station, class_name: 'Station'
 end
