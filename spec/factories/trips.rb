f32fd08772b776fb6aa2d2403ebdfafc32107296
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 12:36:03 -0700

    Create and run migration for trips. Create trip factory. Passing tests.

---
+++ spec/factories/trips.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :trip do
+    duration 1
+    start_date "2018-02-19 12:27:55"
+    end_date "2018-02-19 12:27:55"
+    bike_id 1
+    subscription_type "MyString"
+    zip_code 1
+  end
+end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-describe Trip, type: :model do
-  it { should validate_presence_of(:duration) }
-  it { should validate_presence_of(:start_date) }
-  it { should validate_presence_of(:start_station_id) }
-  it { should validate_presence_of(:end_date) }
-  it { should validate_presence_of(:end_station_id) }
-  it { should validate_presence_of(:bike_id) }
-  it { should validate_presence_of(:subscription_type) }
-  it { should validate_presence_of(:zip_code) }
+RSpec.describe Trip, type: :model do
+  it { should validate_presence_of(:duration)}
+  it { should validate_presence_of(:start_date)}
+  it { should validate_presence_of(:end_date)}
+  it { should validate_presence_of(:bike_id)}
+  it { should validate_presence_of(:subscription_type)}
+  it { should validate_presence_of(:zip_code)}
 end
