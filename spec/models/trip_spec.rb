45133ecefdb0e62acf57ef9d08b8a376cd507948
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 15:23:18 -0700

    Write spec for trip index page.

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Trip, type: :model do
-  it { should validate_presence_of(:duration)}
-  it { should validate_presence_of(:start_date)}
-  it { should validate_presence_of(:end_date)}
-  it { should validate_presence_of(:bike_id)}
-  it { should validate_presence_of(:subscription_type)}
-  it { should validate_presence_of(:zip_code)}
+describe Trip, type: :model do
+  it { should validate_presence_of(:duration) }
+  it { should validate_presence_of(:start_date) }
+  it { should validate_presence_of(:end_date) }
+  it { should validate_presence_of(:bike_id) }
+  it { should validate_presence_of(:subscription_type) }
+  it { should validate_presence_of(:zip_code) }
+
+  describe "relationships" do
+    it { is_expected.to belong_to(:start_station) }
+    it { is_expected.to belong_to(:end_station) }
+  end
 end
