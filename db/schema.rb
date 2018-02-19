92f62ce1dae2fb3cfcd3d2445864fc6d33eb5355
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 12:36:03 -0700

    Create and run migration for trips. Create trip factory. Passing tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219165231) do
+ActiveRecord::Schema.define(version: 20180219192755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 20180219165231) do
     t.text "city"
     t.datetime "installation_date"
     t.string "slug"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "duration"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "bike_id"
+    t.string "subscription_type"
+    t.integer "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
