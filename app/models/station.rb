27304a9e051eb2dc32b6f99f5c920a8d30596094
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 14:20:09 -0700

    Add multiple associations on Trips from Stations via the model level. Passing test.

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,6 +4,9 @@ class Station < ApplicationRecord
 
   validates_presence_of :name, :dock_count, :city, :installation_date
 
+  has_many :start_trips, class_name: "Trip", foreign_key: "start_station_id"
+  has_many :end_trips, class_name: "Trip", foreign_key: "end_station_id"
+
   def self.average_bike_count
     average(:dock_count)
   end
