93689442e4209214c3230ab46d3af261ba0f735d
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 22:26:30 -0700

    Create spec for trip show page.

---
+++ spec/features/trips/visitor_can_see_one_trip_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  before :each do
+    @station = create(:station)
+    @trip = create(:trip)
+  end
+  describe "when I visit a trip show page" do
+    it "I see all attributes for trip" do
+      visit trip_path(@trip)
+
+      expect(page).to have_content(@trip.duration)
+      expect(page).to have_content(@trip.start_date)
+      expect(page).to have_content(@trip.start_station.name)
+      expect(page).to have_content(@trip.end_date)
+      expect(page).to have_content(@trip.end_station.name)
+      expect(page).to have_content(@trip.bike_id)
+      expect(page).to have_content(@trip.subscription_type)
+      expect(page).to have_content(@trip.zip_code)
+    end
+  end
+end
