c6b74ff74a53b5e6877a4f198fd8804f9f8389fc
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 08:48:45 -0700

    Resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   resources :stations
 
+  resources :trips
+
   resources :conditions
 
   resources :users, except: [:index, :destroy, :show]
-  
-  resources :trips
 
   get "/stations-dashboard", :to => "stations_dashboard#index"
 
