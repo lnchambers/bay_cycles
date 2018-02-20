834c1b7ec26c14393621a2e200bf51896ead3bf7
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 22:20:21 -0700

    Add Kiminari gem and edit configs. Add paginate buttons to trip index page. Passing tests.

--- spec/features/trips/visitor_can_see_all_trips_spec.rb
+++ spec/features/trips/visitor_can_see_all_trips_spec.rb
@@ -6,7 +6,7 @@ describe "As a visitor" do
     @trips = create_list(:trip, 60)
   end
   describe "when I visit trips index" do
-    it "I see first 30 trips" do
+    it "I see first 30 trips and click next and see the next 30 trips" do
       visit trips_path
 
       expect(page).to have_content(@trips[0].id)
@@ -18,8 +18,16 @@ describe "As a visitor" do
       expect(page).to have_content(@trips[0].bike_id)
       expect(page).to have_content(@trips[0].subscription_type)
       expect(page).to have_content(@trips[0].zip_code)
+      expect(page).to have_content(@trips[29].id)
+      expect(page).to_not have_content("Trip ID: #{@trips[30].id}")
+
+      visit trips_path
+      click_on "Next"
+      save_and_open_page
+      expect(page).to have_content(@trips[30].id)
       expect(page).to have_content(@trips[30].duration)
-      expect(page).to_not have_content(@trips[31].id)
+      expect(page).to have_content(@trips[59].duration)
+      expect(page).to_not have_content("Trip ID: #{@trips[29].id}")
     end
   end
 end
