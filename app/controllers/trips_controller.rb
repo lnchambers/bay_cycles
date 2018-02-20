3ca697ccc30c2743fbfdc19112d66a77d1f73e36
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 22:35:36 -0700

    Add trip show page. Passing tests.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,16 @@
 class TripsController < ApplicationController
+  before_action :find_trip, only: [:show, :edit, :update, :destroy]
 
   def index
     @trips = Trip.order(:start_date).page params[:page]
+  end
+
+  def show
+  end
+
+  private
+
+  def find_trip
+    @trip = Trip.find(params[:id])
   end
 end
