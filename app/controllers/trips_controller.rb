6338ecceaad5c81a503f679ba0fde7f2d230875a
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 23:07:27 -0700

    Add destroy route for trips. Passing tests.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,6 +8,12 @@ class TripsController < ApplicationController
   def show
   end
 
+  def destroy
+    @trip.destroy
+    flash[:notice] = "Trip deleted =("
+    redirect_to trips_path
+  end
+
   private
 
   def find_trip
