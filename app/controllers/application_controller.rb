c33e8650b81c422d194463b41454f382feca24d0
Luke Chambers <lukenatchambers@gmail.com>
2018-02-20 15:51:09 -0700

    Build out tests for edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :current_admin?
+  helper_method :current_user, :current_admin?, :made_with
 
   def require_admin
     render file: "/public/404" unless current_admin?
@@ -17,5 +17,14 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def made_with
+    [
+    "Malice of Forethought", "Careful Minelaying", "Overflowing Love", "Changing Intentions", "Different Ideas",
+    "Revolving Pinatas", "Generous Crocodiles", "Pure Coffee", "Random Arrays", "Fortunate Cookies",
+    "Goliath Trucks", "Some Code", "5 Year Old's Help", "General Kenobi", "Xenoplastic Morphology", "Ellen's Positivity",
+    "Luis' Artistic Genius", "Luke's Ideaologies"
+    ]
   end
 end
---
+++ spec/features/users/user_can_edit_their_profile_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+describe "As a registered User or Admin" do
+  before :all do
+    @admin = create(:admin)
+    @user = create(:user)
+  end
+  describe "when I visit the edit user path" do
+    it "admin can edit only their own information" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit edit_user_path(@admin)
+
+      expect(page).to have_content("Edit your profile, #{@admin.name}")
+      expect(page).to_not have_content(@user.name)
+    end
+
+    it "user can edit only their own information" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit edit_user_path(@user)
+
+      expect(page).to have_content("Edit your profile, #{@user.name}")
+      expect(page).to_not have_content(@admin.name)
+    end
+
+    it "visitor cannot get to the edit user page" do
+      visit edit_user_path(@user)
+
+      expect(page).to have_content("The page you were looking for was not found")
+    end
+
+    it "I cannot edit another users path" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit edit_user_path(@user)
+
+      expect(page).to have_content("The page you were looking for was not found")
+    end
+  end
+
+  describe "when I visit the user dashboard" do
+    it "admin can see a link to edit their profile" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit admin_dashboard_path
+
+      click_on "Edit Profile"
+
+      expect(current_path).to eq(edit_user_path(@admin))
+      expect(page).to have_content("Edit your profile, #{@admin.name}")
+    end
+
+    it "user can see a link to edit their profile" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit dashboard_path
+
+      click_on "Edit Profile"
+
+      expect(current_path).to eq(edit_user_path(@user))
+      expect(page).to have_content("Edit your profile, #{@user.name}")
+    end
+  end
+end
