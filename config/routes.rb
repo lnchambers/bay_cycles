3c90c138a43fe61819d8b5a6b3787919f6b0dd2b
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 08:48:45 -0700

    Resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :stations
-<<<<<<< HEAD
-  resources :trips  
-=======
-  resources :conditions
 
+  resources :trips
+
+  resources :conditions
+  
   resources :users, except: [:index, :destroy, :show]
->>>>>>> 849ce12bcb4af09d30f59e8e6e2359ca7021c67b
 
   get "/stations-dashboard", :to => "stations_dashboard#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180219202010) do
-=======
-ActiveRecord::Schema.define(version: 20180219224707) do
 
->>>>>>> 849ce12bcb4af09d30f59e8e6e2359ca7021c67b
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
