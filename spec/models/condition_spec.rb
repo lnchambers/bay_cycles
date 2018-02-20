c6c3e06a710e84eedc97ad9c130d6b3a5f80747a
heybluguy <heybluguy@yahoo.com>
2018-02-19 18:00:11 -0700

    Add model tests.

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Condition, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:date)}
+  it { should validate_presence_of(:max_temperature)}
+  it { should validate_presence_of(:mean_temperature)}
+  it { should validate_presence_of(:min_temperature)}
+  it { should validate_presence_of(:mean_humidity)}
+  it { should validate_presence_of(:mean_visibility)}
+  it { should validate_presence_of(:mean_wind_speed)}
+  it { should validate_presence_of(:precipitation)}
 end
