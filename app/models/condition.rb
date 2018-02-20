849ce12bcb4af09d30f59e8e6e2359ca7021c67b
heybluguy <heybluguy@yahoo.com>
2018-02-19 18:00:11 -0700

    Add model tests.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,2 +1,4 @@
 class Condition < ApplicationRecord
+  validates_presence_of :date, :max_temperature, :mean_temperature, :min_temperature, :mean_humidity, :mean_visibility, :mean_wind_speed, :precipitation
+
 end
