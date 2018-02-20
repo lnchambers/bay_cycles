ee15987253bb15197c5c42b3a9ce4b4c8e050f8f
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 07:44:45 -0700

    Create visitor authroization test for trip edit and delete.

---
+++ spec/features/trips/visitor_cannot_edit_or_delete_a_trip_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  before :all do
+    @station = create(:station)
+    @trip = create(:trip)
+  end
+
+  describe "when I visit the trips index page" do
+    it "I do not see edit or delete buttons next to each trip" do
+      visit trips_path
+
+      expect(page).to_not have_content("Delete")
+      expect(page).to_not have_content("Edit")
+    end
+  end
+
+  describe "when I try to visit trip/:id/edit" do
+    it "I am redirected to a 404 page" do
+      visit edit_trip_path(@trip)
+
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+
+  describe "when I try to visit trips/new" do
+    it "I am redirected to a 404 page" do
+      visit new_trip_path
+
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end
