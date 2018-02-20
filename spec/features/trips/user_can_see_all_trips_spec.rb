36939c26f72878c2185f2e5b37c3febac5f9bff4
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-19 17:18:35 -0700

    Modify trips features test. Add trips controller and index view.

--- spec/features/trips/user_can_see_all_trips_spec.rb
+++ spec/features/trips/user_can_see_all_trips_spec.rb
@@ -9,7 +9,17 @@ describe "As a visitor" do
     it "I see first 30 trips" do
       visit trips_path
 
-      expect(page).to have_content(@trips[0..30].map {|trip| trip.duration})
+      expect(page).to have_content(@trips[0].id)
+      expect(page).to have_content(@trips[0].duration)
+      expect(page).to have_content(@trips[0].start_date)
+      expect(page).to have_content(@trips[0].start_station.name)
+      expect(page).to have_content(@trips[0].end_date)
+      expect(page).to have_content(@trips[0].end_station.name)
+      expect(page).to have_content(@trips[0].bike_id)
+      expect(page).to have_content(@trips[0].subscription_type)
+      expect(page).to have_content(@trips[0].zip_code)
+      expect(page).to have_content(@trips[30].duration)
+      expect(page).to_not have_content(@trips[31].id)
     end
   end
 end
