182053336ab5f08907bef2702a21e579dbab1130
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 07:55:06 -0700

    Create spec for admin edit trip.

---
+++ spec/features/trips/admin_can_edit_a_trip_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "As an Admin" do
+  before :all do
+    @station = create(:station)
+    @trip = create(:trip)
+  end
+  before :each do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
+  describe "when I visit the trips show page and click edit and fill in form" do
+    it "I am redirected to the trip's show page and see trip's updated info" do
+      visit trip_path(@trip)
+      click_on "Edit"
+      fill_in "Duration", with: "100"
+      click_on "Update"
+
+      expect(current_path).to eq(trip_path(@trip))
+      expect(page).to have_content("100")
+    end
+  end
+end
