67b26a651973b7c383fa70623c76ccec7429b643
heybluguy <heybluguy@yahoo.com>
2018-02-21 00:18:46 -0700

    Add admin new form to create a new condition.

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -8,5 +8,23 @@ class ConditionsController < ApplicationController
     @condition = Condition.find(params[:id])
   end
 
+  def new
+    @condition = Condition.new
+  end
 
+  def create
+    @condition = Condition.new(condition_params)
+    if @condition.save
+      flash[:notice] = "Condition created"
+      redirect_to condition_path(@condition)
+    else
+      flash[:notice] = "Condition not created. Try again."
+      render :new
+    end
+  end
+
+    private
+      def condition_params
+        params.require(:condition).permit(:date, :max_temperature, :mean_temperature, :min_temperature, :mean_humidity, :mean_visibility, :mean_wind_speed, :precipitation)
+      end
 end
---
+++ spec/features/conditions/admin_can_create_a_new_condition_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe 'As an Admin' do
+  before :each do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
+  describe 'when I visit new conditions path' do
+    it 'I can create a new condition' do
+      visit new_condition_path
+
+      fill_in "condition[date]",	           with:  "2018-02-19 15:47:07"
+      fill_in "condition[max_temperature]",	 with:  1
+      fill_in "condition[mean_temperature]", with:  1
+      fill_in "condition[min_temperature]",	 with:  1
+      fill_in "condition[mean_humidity]",	   with:  1
+      fill_in "condition[mean_visibility]",	 with:  1
+      fill_in "condition[mean_wind_speed]",	 with:  1
+      fill_in "condition[precipitation]",	   with:  1
+    end
+
+    it "I can see a flash message for success" do
+      visit new_condition_path
+
+      fill_in "condition[date]",             with: "2018-02-19 15:47:07"
+      fill_in "condition[max_temperature]",  with: 1
+      fill_in "condition[mean_temperature]", with: 1
+      fill_in "condition[min_temperature]",  with: 1
+      fill_in "condition[mean_humidity]",    with: 1
+      fill_in "condition[mean_visibility]",  with: 1
+      fill_in "condition[mean_wind_speed]",  with: 1
+      fill_in "condition[precipitation]",    with: 1
+      click_on "Create Condition"
+
+      expect(page).to have_content("Condition created")
+    end
+
+    it "I can see a flash message for failure" do
+      visit new_condition_path
+
+      fill_in "condition[max_temperature]",  with: 1
+      fill_in "condition[mean_temperature]", with: 1
+      fill_in "condition[min_temperature]",  with: 1
+      fill_in "condition[mean_humidity]",    with: 1
+      fill_in "condition[mean_visibility]",  with: 1
+      fill_in "condition[mean_wind_speed]",  with: 1
+      fill_in "condition[precipitation]",    with: 1
+      click_on "Create Condition"
+
+      expect(page).to have_content("Condition not created. Try again.")
+    end
+  end
+end
