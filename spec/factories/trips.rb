d685f6b36d815a4ce01f222d84dbe8d23895ac8b
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 22:53:15 -0700

    Create spec for trips class methods. Modify Trip Factory Bot to sequence.

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
 
   factory :trip do
-    duration 1
     start_date "2018-02-19 12:27:55"
     end_date "2018-02-19 12:27:55"
     bike_id 1
@@ -9,5 +8,7 @@ FactoryBot.define do
     zip_code 1
     start_station_id 1
     end_station_id 1
+    
+    sequence(:duration) {|n| n }
   end
 end
