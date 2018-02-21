4168a2134d405746cf013fc27d6fb103d51098a1
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-20 23:38:12 -0700

    Continue working on ActiveRecord methods for trips.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,28 @@ class Trip < ApplicationRecord
 
   belongs_to :start_station, class_name: 'Station'
   belongs_to :end_station, class_name: 'Station'
+
+  def self.average_duration
+    average(:duration)
+  end
+
+  def self.shortest_ride
+    minimum(:duration)
+  end
+
+  def self.longest_ride
+    maximum(:duration)
+  end
+
+  def self.start_station_ordered_by_rides
+    select("start_station_id, count(start_station_id)")
+    .group("start_station_id")
+    .order("count(start_station_id) desc")
+  end
+
+  def self.end_station_ordered_by_rides
+    select("end_station_id, count(end_station_id)")
+    .group("end_station_id")
+    .order("count(end_station_id) desc")
+  end
 end
