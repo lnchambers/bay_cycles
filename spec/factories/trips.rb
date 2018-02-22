ab2b9a2a222cb7b98c10927d1bf1a96de2ce86e6
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-21 21:48:41 -0700

    Refactor trip model tests and methods. Passing tests.

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
     start_date "2018-02-19 12:27:55"
     end_date "2018-02-19 12:27:55"
     bike_id 1
-    subscription_type "MyString"
+    subscription_type "Subscriber"
     zip_code 1
     start_station_id 1
     end_station_id 1
-    
+
     sequence(:duration) {|n| n }
   end
 end
