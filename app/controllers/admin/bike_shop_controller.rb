f018e9225af19de96083c3f16e17c9c631c3102b
Luke Chambers <lukenatchambers@gmail.com>
2018-02-25 16:40:44 -0700

    Build out accessories crud

--- app/controllers/admin/bike_shop_controller.rb
+++ app/controllers/admin/bike_shop_controller.rb
@@ -1,7 +1,8 @@
 class Admin::BikeShopController < Admin::BaseController
 
   def index
-    @accessories = Accessory.all
+    @active_accessories = Accessory.where(role: 0)
+    @inactive_accessories = Accessory.where(role: 1)
   end
 
 end
--- spec/features/accessories/admin_can_view_all_accessories_spec.rb
+++ spec/features/accessories/admin_can_view_all_accessories_spec.rb
@@ -3,15 +3,26 @@ require "rails_helper"
 describe "As an admin" do
   before :each do
     @admin = create(:admin)
+    @accessory_active = create(:accessory)
+    @accessory_inactive = create(:accessory, name: "I don't work", role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
   end
   describe "when I visit the admin/dashboard" do
+
     it "I can see a link to view all accessories" do
       visit admin_dashboard_path
 
       click_on "View Accessories"
 
       expect(current_path).to eq("/admin/bike-shop")
+    end
+
+    it "I can see a table with all accessories" do
+      visit admin_bike_shop_path
+
+      expect(page).to have_content(@accessory_active.name)
+      expect(page).to have_content(@accessory_inactive.name)
     end
   end
 end
