fc3aa343ae41662485e4f160867399e9d8017c57
Luke Chambers <lukenatchambers@gmail.com>
2018-02-25 16:21:57 -0700

    Build out first passing test

--- app/controllers/admin/bike_shop_controller.rb
+++ app/controllers/admin/bike_shop_controller.rb
@@ -1,3 +1,7 @@
 class Admin::BikeShopController < Admin::BaseController
 
+  def index
+    @accessories = Accessory.all
+  end
+
 end
--- spec/features/accessories/admin_can_view_all_accessories_spec.rb
+++ spec/features/accessories/admin_can_view_all_accessories_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 describe "As an admin" do
+  before :each do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
   describe "when I visit the admin/dashboard" do
     it "I can see a link to view all accessories" do
       visit admin_dashboard_path
@@ -8,3 +12,6 @@ describe "As an admin" do
       click_on "View Accessories"
 
       expect(current_path).to eq("/admin/bike-shop")
+    end
+  end
+end
