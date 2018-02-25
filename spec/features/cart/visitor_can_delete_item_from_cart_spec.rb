5b4874bd02f3150b2a3430b6ba8c61a9d6bf1bcc
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-25 11:21:01 -0700

    Add decrease quantity function for cart accessories.

--- spec/features/cart/visitor_can_delete_item_from_cart_spec.rb
+++ spec/features/cart/visitor_can_delete_item_from_cart_spec.rb
@@ -65,5 +65,55 @@ describe "As a visitor" do
         expect(page).to have_content("Accessory: Small Hoops")
       end
     end
+
+    describe "I decrease the quanity of an item" do
+      it "I see the item's quantity go down" do
+        visit bike_shop_path
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        visit carts_path
+
+        expect(page).to have_content("Cart(3)")
+        expect(page).to have_content("Quantity: 3")
+        expect(page).to have_content("Subtotal: $30000")
+
+        click_on "Decrease Quantity"
+
+        expect(current_path).to eq(carts_path)
+        expect(page).to have_content("Cart(2)")
+        expect(page).to have_content("Quantity: 2")
+        expect(page).to have_content("Subtotal: $20000")
+      end
+    end
+
+    describe "I decrease the quanity of an item to 1" do
+      it "I don't see the decrease button anymore" do
+        visit bike_shop_path
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        within ".accessory#{@accessory_1.id}" do
+          click_on "Add to Cart"
+        end
+        visit carts_path
+        click_on "Decrease Quantity"
+
+        expect(page).to have_button("Decrease Quantity")
+
+        click_on "Decrease Quantity"
+
+        expect(page).to_not have_button("Decrease Quantity")
+      end
+    end
   end
 end
