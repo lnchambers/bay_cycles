8da8f7aa2a2ffd75f50e9a1efd6a9fa0447a9dff
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 12:39:02 -0700

    Model tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226195733) do
+ActiveRecord::Schema.define(version: 20180226193434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 20180226195733) do
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.text "purchaser_name"
+    t.text "purchaser_address"
+    t.text "item_name"
+    t.text "item_quantity"
+    t.integer "item_price"
+    t.text "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "stations", force: :cascade do |t|
---
+++ spec/factories/orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order do
+    purchaser_name "MyText"
+    purchaser_address "MyText"
+    item_name "MyText"
+    item_quantity "MyText"
+    item_price 1
+    status "MyText"
+  end
+end
