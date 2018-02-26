6ccb206c72a30b1227b8c97d8a964a465d269abb
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 14:04:21 -0700

    First user story is passing

--- db/migrate/20180226193434_create_orders.rb
+++ db/migrate/20180226193434_create_orders.rb
@@ -3,9 +3,6 @@ class CreateOrders < ActiveRecord::Migration[5.1]
     create_table :orders do |t|
       t.text :purchaser_name
       t.text :purchaser_address
-      t.text :item_name
-      t.text :item_quantity
-      t.integer :item_price
       t.text :status
 
       t.timestamps
