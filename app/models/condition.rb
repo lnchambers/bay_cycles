e54694209b7082b21253f18e84f42ba064e3a84c
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-26 13:14:19 -0700

    Add tests and methods for wind speeds. Passing tests.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -43,4 +43,25 @@ class Condition < ApplicationRecord
   def self.lowest_rides_for_precipitation(range)
     precipitation_ranges(range).min
   end
+
+  def self.wind_speed_ranges(range)
+    joins(:trips)
+    .where("mean_wind_speed >= #{range[0]} AND mean_wind_speed <= #{range[1]}")
+    .group(:condition_id)
+    .count(:condition_id)
+    .values
+  end
+
+  def self.average_rides_for_wind(range)
+    breakdown_rides = wind_speed_ranges(range)
+    (breakdown_rides.sum / breakdown_rides.size.to_f).round(1)
+  end
+
+  def self.most_rides_for_wind(range)
+    wind_speed_ranges(range).max
+  end
+
+  def self.lowest_rides_for_wind(range)
+    wind_speed_ranges(range).min
+  end
 end
