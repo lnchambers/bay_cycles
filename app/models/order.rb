9d0ade6b997b630a5590c195c506ae0d779e092d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 12:39:02 -0700

    Model tests passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,3 @@
 class Order < ApplicationRecord
-  validates_presence_of :purchaser_name, :purchaser_address, :status, :user_id
-  belongs_to :user
-  has_many :ordered_accessories
-  has_many :accessories, through: :ordered_accessories
-
-  def total_cost
-    ordered_accessories.sum do |ordered_accessory|
-      Accessory.find(ordered_accessory.accessory_id).price * ordered_accessory.quantity
-    end
-  end
+  validates_presence_of :purchaser_name, :purchaser_address, :item_name, :item_quantity, :item_price, :status
 end
