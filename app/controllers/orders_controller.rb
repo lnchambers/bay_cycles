2d4996b13c81ac36beb0521524825fbdcef24a3e
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 14:04:21 -0700

    First user story is passing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,12 @@
 class OrdersController < ApplicationController
 
+  def show
+    @order = Order.find(params[:id])
+  end
+
   def new
     if current_user
-
+      @order = Order.new
     else
       flash[:notice] = "Please login first"
       redirect_to login_path
@@ -11,11 +15,24 @@ class OrdersController < ApplicationController
 
   def create
     if current_user
-      flash[:notice] = "You have successfully submitted your order"
-      redirect_to dashboard_path(current_user)
+      @order = Order.new(order_params)
+      @order.user_id = params[:user_id].keys[0]
+      @order.status = params[:status].keys[0]
+      if @order.save
+        flash[:notice] = "You have successfully submitted your order!"
+        redirect_to dashboard_path(current_user)
+      else
+
+      end
     else
       redirect_to login_path
     end
   end
+
+  private
+
+    def order_params
+      params.require(:order).permit(:purchaser_name, :purchaser_address)
+    end
 
 end
