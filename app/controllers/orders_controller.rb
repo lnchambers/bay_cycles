a71954a201f336f08621b1ae2826c1d25e15095d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 13:22:12 -0700

    Build out orders scaffolding

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
 
+  def new
+    if current_user
+
+    else
+      flash[:notice] = "Please login first"
+      redirect_to login_path
+    end
+  end
+
   def create
     if current_user
       flash[:notice] = "You have successfully submitted your order"
