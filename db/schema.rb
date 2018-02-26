1b2a5072fb751922919c1700af0f15a21902fc25
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-26 11:45:24 -0700

    Remove foreign key constraint on trips.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226034409) do
+ActiveRecord::Schema.define(version: 20180226183753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,4 @@ ActiveRecord::Schema.define(version: 20180226034409) do
     t.datetime "updated_at", null: false
     t.string "slug"
   end
-
-  add_foreign_key "trips", "conditions"
 end
