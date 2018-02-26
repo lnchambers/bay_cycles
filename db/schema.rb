a8f374739e4fd212dd5eca2f3c1d48ea0314caa4
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 13:04:40 -0700

    Start building out orders crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226193434) do
+ActiveRecord::Schema.define(version: 20180226195450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20180226193434) do
     t.text "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "stations", force: :cascade do |t|
@@ -95,4 +97,5 @@ ActiveRecord::Schema.define(version: 20180226193434) do
     t.string "slug"
   end
 
+  add_foreign_key "orders", "users"
 end
