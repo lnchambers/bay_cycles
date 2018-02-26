f1a2f09c9290c0f01de8400d853d51f9c0c195a2
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 14:04:21 -0700

    First user story is passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
-  validates_presence_of :purchaser_name, :purchaser_address, :item_name, :item_quantity, :item_price, :status, :user_id
+  validates_presence_of :purchaser_name, :purchaser_address, :status, :user_id
   belongs_to :user
 end
