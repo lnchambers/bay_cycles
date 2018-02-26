cfade1b8d554deee3111de86bf154c3f9f86b3de
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 11:55:04 -0700

    Added flash notice

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def create
     if current_user
+      flash[:notice] = "You have successfully submitted your order"
       redirect_to dashboard_path(current_user)
     else
       redirect_to login_path
