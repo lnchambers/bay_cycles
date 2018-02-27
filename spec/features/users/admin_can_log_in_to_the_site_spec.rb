2eed02d2531a32f851394c4ed43acfcc3bf51123
Luke Chambers <lukenatchambers@gmail.com>
2018-02-27 14:13:25 -0700

    Second part of user story passing

--- spec/features/users/admin_can_log_in_to_the_site_spec.rb
+++ spec/features/users/admin_can_log_in_to_the_site_spec.rb
@@ -5,6 +5,9 @@ describe "As a registered User" do
     @admin = create(:admin)
     @order = create(:order, user: @admin)
     @order_2 = create(:order, user: @admin)
+    @order_3 = create(:order, user: @admin, status: "Paid")
+    @order_4 = create(:order, user: @admin, status: "Cancelled")
+    @order_5 = create(:order, user: @admin, status: "Completed")
   end
   describe "when I visit the root path" do
     it "I can click on Login and login to my account" do
@@ -35,6 +38,16 @@ describe "As a registered User" do
 
       expect(page).to have_content("Order ##{@order.id}")
       expect(page).to have_content("Order ##{@order_2.id}")
+    end
+
+    it "I can see a total number of orders for each status" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit admin_dashboard_path
+
+      expect(page).to have_content("Ordered: 2")
+      expect(page).to have_content("Paid: 1")
+      expect(page).to have_content("Cancelled: 1")
+      expect(page).to have_content("Completed: 1")
     end
   end
 end
