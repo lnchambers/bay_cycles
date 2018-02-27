a51534336fc7798f6e0b3661cb950c0c5e8786eb
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 23:29:45 -0700

    Test passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :ordered_accessories
   has_many :accessories, through: :ordered_accessories
+
+  def total_cost
+    ordered_accessories.sum do |ordered_accessory|
+      Accessory.find(ordered_accessory.accessory_id).price * ordered_accessory.quantity
+    end
+  end
 end
