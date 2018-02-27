3048be58d9d2dc4e2ae3a01197510763a28cd030
Luke Chambers <lukenatchambers@gmail.com>
2018-02-27 13:58:40 -0700

    First part of test passing

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < Admin::BaseController
   def show
-    @user = current_user
+    @orders = Order.all
   end
 end
--- spec/features/users/admin_can_log_in_to_the_site_spec.rb
+++ spec/features/users/admin_can_log_in_to_the_site_spec.rb
@@ -1,24 +1,40 @@
 require "rails_helper"
 
 describe "As a registered User" do
+  before :each do
+    @admin = create(:admin)
+    @order = create(:order, user: @admin)
+    @order_2 = create(:order, user: @admin)
+  end
   describe "when I visit the root path" do
     it "I can click on Login and login to my account" do
-      admin = create(:admin)
       visit root_path
 
       click_on "Login"
 
-      fill_in "email", with: admin.email
-      fill_in "password", with: admin.password
+      fill_in "email", with: @admin.email
+      fill_in "password", with: @admin.password
 
       click_on "Submit"
 
       expect(current_path).to eq(admin_dashboard_path)
-      expect(page).to have_content("Welcome, oh Mighty #{admin.name}. Your realm awaits.")
-      expect(page).to have_content("Logged in as #{admin.name}")
-      expect(page).to have_content("Current Email: #{admin.email}")
+      expect(page).to have_content("Welcome, oh Mighty #{@admin.name}. Your realm awaits.")
+      expect(page).to have_content("Logged in as #{@admin.name}")
+      expect(page).to have_content("Current Email: #{@admin.email}")
       expect(page).to have_content("Logout")
       expect(page).to_not have_content("Login")
+    end
+
+    it "I can expect true to return true" do
+      expect(true).to eq(true)
+    end
+
+    it "I can see a list of all orders" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit admin_dashboard_path
+
+      expect(page).to have_content("Order ##{@order.id}")
+      expect(page).to have_content("Order ##{@order_2.id}")
     end
   end
 end
