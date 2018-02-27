904ee53d4ae959782c070a146a6cb8fbf26c16f8
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 23:13:27 -0700

    Admin can view the order show page

--- spec/features/orders/admin_can_see_a_specific_order_spec.rb
+++ spec/features/orders/admin_can_see_a_specific_order_spec.rb
@@ -6,13 +6,21 @@ describe "As an Admin" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     @order = create(:order)
     @accessory = create(:accessory)
-    OrderedAccessories.create!(order_id: @order.id, accessory_id: @accessory.id, quantity: 4)
+    OrderedAccessory.create!(order: @order, accessory: @accessory, quantity: 4)
+    visit admin_order_path(@order)
   end
   describe "when I visit the admin/order/:id page" do
     it "I can see the order's date and time" do
-      visit admin_order_path(@order)
-
       expect(page).to have_content(@order.updated_at)
+    end
+
+    it "I can see the purchaser's name and address" do
+      expect(page).to have_content(@order.purchaser_name)
+      expect(page).to have_content(@order.purchaser_address)
+    end
+
+    it "I can see the accessories ordered" do
+      expect(page).to have_content(@accessory.name)
     end
   end
 end
