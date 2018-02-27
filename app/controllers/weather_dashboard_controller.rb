be2a7124b9cb93f01d6c097b299bd3ae3bdbdf04
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-26 17:36:25 -0700

    Finish styling weather deashboard.

--- app/controllers/weather_dashboard_controller.rb
+++ app/controllers/weather_dashboard_controller.rb
@@ -4,6 +4,8 @@ class WeatherDashboardController < ApplicationController
   def index
     @temp_ranges = [[50, 59.9], [60, 69.9], [70, 79.9]]
     @precipitation_ranges = [[0, 0.49], [0.5, 0.99]]
+    @wind_ranges = [[0, 3.99], [4, 7.99]]
+    @visibility_ranges = [[0, 3.99], [4, 7.99]]
     @conditions = Condition.all
   end
 end
