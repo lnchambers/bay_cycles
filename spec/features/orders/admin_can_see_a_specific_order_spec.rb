0e73311777a949fd2e51c643067419d27b14266d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-26 23:29:45 -0700

    Test passing

--- spec/features/orders/admin_can_see_a_specific_order_spec.rb
+++ spec/features/orders/admin_can_see_a_specific_order_spec.rb
@@ -6,7 +6,9 @@ describe "As an Admin" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     @order = create(:order)
     @accessory = create(:accessory)
+    @accessory_2 = create(:accessory)
     OrderedAccessory.create!(order: @order, accessory: @accessory, quantity: 4)
+    OrderedAccessory.create!(order: @order, accessory: @accessory_2, quantity: 2)
     visit admin_order_path(@order)
   end
   describe "when I visit the admin/order/:id page" do
@@ -21,14 +23,21 @@ describe "As an Admin" do
 
     it "I can see the accessories ordered" do
       expect(page).to have_content(@accessory.name)
+      expect(page).to have_content(@accessory_2.name)
     end
 
     it "I can see the total quantity of an accessory ordered" do
       expect(page).to have_content("Quantity: 4")
+      expect(page).to have_content("Quantity: 2")
     end
 
     it "I see the line item subtotal" do
-      expect(page).to have_content("Line Item Subtotal: $10,000")
+      expect(page).to have_content("Line Item Subtotal: $40,000")
+      expect(page).to have_content("Line Item Subtotal: $20,000")
+    end
+
+    it "I also see the total for the order" do
+      expect(page).to have_content("Total Cost: $60,000")
     end
   end
 end
