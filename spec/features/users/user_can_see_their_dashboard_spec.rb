b70f6bf2bc756df7a26f2c72595b531024d7ac13
Luke Chambers <lukenatchambers@gmail.com>
2018-02-27 20:36:23 -0700

    Second test of user story passing

--- spec/features/users/user_can_see_their_dashboard_spec.rb
+++ spec/features/users/user_can_see_their_dashboard_spec.rb
@@ -4,7 +4,12 @@ describe "As a registered User" do
   before :each do
     @user = create(:user)
     @order = create(:order, user: @user)
+    @accessory = create(:accessory)
+    @accessory_2 = create(:accessory)
+    OrderedAccessory.create!(order: @order, accessory: @accessory, quantity: 4)
+    OrderedAccessory.create!(order: @order, accessory: @accessory_2, quantity: 2)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit order_path(@order)
   end
   describe "when I visit the /dashboard path" do
     it "I can click on one of my orders" do
@@ -13,6 +18,13 @@ describe "As a registered User" do
       click_on "Order ##{@order.id}"
 
       expect(current_path).to eq(order_path(@order))
+    end
+
+    it "I can see a list of accessories I ordered broken down by subtotal and quantity" do
+      expect(page).to have_content("Quantity: 4")
+      expect(page).to have_content("Quantity: 2")
+      expect(page).to have_content("Subtotal: $40,000")
+      expect(page).to have_content("Subtotal: $20,000")
     end
   end
 end
